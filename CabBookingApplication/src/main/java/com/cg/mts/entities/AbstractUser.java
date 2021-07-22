68abdeb98ed25b3e08b2448fc99c1760a8ea7c37
Genesis801 <debajyotiroy21@gmail.com>
2021-07-22 15:35:21 +0530

    Abstract User Table created

--- CabBookingApplication/src/main/java/com/cg/mts/entities/AbstractUser.java
+++ CabBookingApplication/src/main/java/com/cg/mts/entities/AbstractUser.java
@@ -1,22 +1,44 @@
 package com.cg.mts.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.SequenceGenerator;
+
+
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class AbstractUser {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "order_Sequence")
+    @SequenceGenerator(name = "order_Sequence", sequenceName = "ORDER_SEQ")
+	private long joinId;
+	
 	private String username;
 	private String password;
 	private String mobileNumber;
 	private String email;
 	
 	public AbstractUser() {
-		super();
+
 	}
 
-	public AbstractUser(String username, String password, String mobileNumber, String email) {
-		super();
+
+	public AbstractUser(long joinId, String username, String password, String mobileNumber, String email) {
+
+		this.joinId = joinId;
 		this.username = username;
 		this.password = password;
 		this.mobileNumber = mobileNumber;
 		this.email = email;
 	}
+
+
 
 	public String getUsername() {
 		return username;
@@ -48,5 +70,17 @@ public abstract class AbstractUser {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+
+
+	public long getJoinId() {
+		return joinId;
+	}
+
+
+
+	public void setJoinId(long joinId) {
+		this.joinId = joinId;
 	}
 }
