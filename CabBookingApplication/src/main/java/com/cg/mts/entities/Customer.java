9fbf8032614b93d288739fc429e4a61cd6110cea
Genesis801 <debajyotiroy21@gmail.com>
2021-07-27 16:41:14 +0530

    Customer entity modified

--- CabBookingApplication/src/main/java/com/cg/mts/entities/Customer.java
+++ CabBookingApplication/src/main/java/com/cg/mts/entities/Customer.java
@@ -1,8 +1,11 @@
 package com.cg.mts.entities;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -14,13 +17,26 @@ public class Customer extends AbstractUser{
 //    @SequenceGenerator(name = "customer_Sequence", sequenceName = "CUSTOMER_SEQ", initialValue = 201)
 	//private int customerId;
 
+	@OneToOne(cascade =  CascadeType.ALL)
+	@JoinColumn(name="tripBookingId")
+	private TripBooking tripBooking; 
+	
 	public Customer() {
 		super();
 	}
 
-	
-	public Customer(long joinId, String username, String password, String mobileNumber, String email, int customerId) {
-		super(joinId, username, password, mobileNumber, email);
+	public Customer(long Id, String username, String password, String mobileNumber, String email,
+			TripBooking tripBooking) {
+		super(Id, username, password, mobileNumber, email);
+		this.tripBooking = tripBooking;
+	}
+
+	public TripBooking getTripBooking() {
+		return tripBooking;
+	}
+
+	public void setTripBooking(TripBooking tripBooking) {
+		this.tripBooking = tripBooking;
 	}
 
 
